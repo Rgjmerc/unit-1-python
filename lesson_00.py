cfc1a56259d053fe0e878b875a69a3f5f2e80a8a
Logan Vergara <lvergara@brooklynsteamcenter.org>
2024-09-16 10:47:18 -0400

    lesson 1

---
+++ lesson_00.py
@@ -0,0 +1,26 @@
+#Step 1: Print Name
+print("Logan")
+#Step 2: Var with fav show name
+Fav_Show = "Blue Lock"
+#Step 3: Create a float and intiger variable
+intiger = 3
+float = 1.2
+#Step 4: Print the product of the number variables
+print(intiger * float)
+#Step 5: Variable with fav foods
+Fav_Foods = ["Pizza", "Hamburger", "Steak"]
+#Step 6: If statement to tell someone if they can get liscence
+age = int(input ("What is your age"))
+if age >= 16:
+    print ("your are old enough to get a license")
+else:
+    print ("You arent old enough to get a license")
+#Step 7: Ask for and print name
+name = input("What is your name")
+print (name)
+#Step 8: Print fav foods with for loop
+for x in Fav_Foods:
+    print (x)
+#Step 9: Create a while loop that never runs
+while intiger * float == 9:
+    float = 7
