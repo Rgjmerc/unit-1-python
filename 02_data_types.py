037dd281dbe4f4b644686f0e53188c97267a1608
Logan Vergara <lvergara@brooklynsteamcenter.org>
2024-09-19 08:59:49 -0400

    comments

--- 02_data_types.py
+++ 02_data_types.py
@@ -6,10 +6,12 @@ The password should not be case sensitive
 """
 Password = "hello"
 Entr_Pswd = input("Enter Password ").lower()
+#input
 if Entr_Pswd == Password:
     print ("Correct")
 else:
     print("Incorrect")
+    #correct or incorrect
 
 """
 TASK 2:
@@ -18,7 +20,9 @@ If the string is empty, print "invalid" otherwise print "valid"
 """
 smth=input("Type something ")
 smths=smth.strip()
+#stripping spaces
 if bool(smths) == True:
+    #converting into bool for text enter check
     print("Valid")
 else:
     print("Invalid")
@@ -40,6 +44,7 @@ Write a program that takes a person's name and age as input and prints it
 """
 age=int(input("Enter age "))
 txt = f"You are {age} years old."
+#inputting variable into the sting
 print (txt)
 
 """
@@ -52,5 +57,7 @@ The result should be rounded to the nearest tenth (1 decimal place)
 decimal1 = float(input("Give me a decimal number "))
 decimal2 = float(input("Give me another smaller decimal number "))
 answer = decimal1 / decimal2
+#doing math
 quotent = f"{decimal1} / {decimal2} = {answer:.1f}"
+#converting into 1 decimal
 print(quotent)
