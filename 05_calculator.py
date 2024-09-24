03e562a86f92b01a79eb0e636b86f812f992c50a
Logan Vergara <lvergara@brooklynsteamcenter.org>
2024-09-24 10:55:45 -0400

    unit 6

--- 05_calculator.py
+++ 05_calculator.py
@@ -4,6 +4,7 @@ print("")
 x = float(input("Enter First Number: "))
 print("")
 y = float(input("Enter Second Number: "))
+#entering numbers
 print("")
 print("Select Operation:")
 print("1. Addition")
@@ -14,25 +15,34 @@ print("5. Floor division")
 print("6. Exponential")
 print("7. Remainder")
 print("")
+#all choices
 choice = int(input("Enter Choice: "))
+#choice
 if choice == 1:
     addition = x+y
     print (addition)
+    #addition
 elif choice == 2:
     subtraction = x-y
     print (subtraction)
+    #subtraction
 elif choice == 3:
     multiplication = x*y
     print (multiplication)
+    #multiplication
 elif choice == 4:
     division = x/y
     print (division)
+    #division
 elif choice == 5:
     floor = x//y
     print (floor)
+    #floor
 elif choice == 6:
     expo = x**y
     print (expo)
+    #exponent
 elif choice == 7:
     remainder = x%y
     print (remainder)
+    #remainder
---
+++ 06_whimsical_whiles.py
@@ -0,0 +1,62 @@
+"""
+1. Simple Counter:
+Write a program that uses a while loop to print numbers from 1 to 10.
+"""
+
+x=1
+while x<=10:
+    print (x)
+    x+=1
+    #forward
+print("")
+
+"""
+2. Countdown:
+Write a program that uses a while loop to print numbers from 10 to 1 in descending order.
+"""
+
+y=10
+while y>=1:
+    print (y)
+    y=y-1
+    #reverse
+print("")
+
+"""
+3. Factorial Calculation:
+Write a program that calculates the factorial of a given number using a while loop.
+"""
+
+factorial= int(input("Enter a number "))
+number=factorial-1
+while number>1:
+    factorial=factorial * number
+    number = number - 1
+    #math for factorial
+print (factorial)
+
+"""
+4. Password Guessing Game:
+Create a simple password guessing game using a while loop. Ask the user to guess a predefined password and provide appropriate feedback.
+"""
+
+pswd_attempt=input("Enter a password attempt ").lower
+password = "password"
+while pswd_attempt != password:
+    print ("Incorrect Password")
+    pswd_attempt=input("Enter a password attempt ")
+    #loops attempts until correct
+else:
+    print("Correct Password")
+
+"""
+5. Sum of Digits:
+Write a program that calculates the sum of the digits of a given number using a while loop.
+"""
+
+
+"""
+6. Fibonacci Series:
+Write a program that prints the first n numbers in the Fibonacci sequence using a while loop.
+"""
+
