209ceba489c487bc6ef0864f374f17382c31fb10
Logan Vergara <lvergara@brooklynsteamcenter.org>
2024-10-10 10:54:19 -0400

    docstrings

--- 12_fabulous_functions.py
+++ 12_fabulous_functions.py
@@ -5,7 +5,10 @@ Write a function that takes a name as a
 parameter and prints a greeting message like "Hello, [name]!".
 """
 
-def greeter(name, greeting="Hello"): 
+def greeter(name, greeting="Hello"):
+    """
+    Greets users with a greeting word and name
+    """
     print( greeting + " " + name )
 greeter("Chimmychangas", "Greetings")
 
@@ -15,6 +18,9 @@ Write a function that takes an integer as an argument and returns its square.
 """
 
 def square(num):
+    """
+    Finds and returns the square of a number
+    """
     return num * num
 print(square(5))
 
@@ -25,6 +31,11 @@ returns `True` if the number is even, and `False` otherwise.
 """
 
 def even_odd(digit):
+    """
+    Determines if a certain number is even or odd
+     
+    Returns True for Even and False for Odd
+    """
     if digit % 2:
         return True
     else:
@@ -37,6 +48,9 @@ Write a function that takes the length and width of a rectangle and returns its 
 """
 
 def area(length, width):
+    """
+    Finds the area of a rectangle from length and width
+    """
     return length * width
 print (area(5,7))
 
@@ -47,6 +61,9 @@ the equivalent temperature in Fahrenheit using the correct formula
 """
 
 def degree_tran(celcius):
+    """
+    Converts degrees frim celcius to farenheit
+    """
     fahrenheit = (celcius * (9/5)) + 32
     return fahrenheit
 print(degree_tran(8))
@@ -57,6 +74,16 @@ Write a function that takes a list of numbers
 and returns the average (mean) of those numbers.
 """
 
+def mean(average):
+    """
+    Finds the mean of given numbers in a list
+    """
+    total = 0
+    for num in average:
+        total += num
+    return total/len(average)
+
+print(mean([1,2,3,4,5]))
 
 
 """
@@ -64,3 +91,10 @@ Task 7: Total Calculator
 Create a function that has arguments for the price and quantity of something, and returns the total.
 Your function should also optionally accept a 3rd argument for discount, and return the discounted if provided.
 """
+
+def calculator(price, quantity, discount = 1.0):
+    """
+    Calculates price and provides a discount
+    """
+    return (price * quantity) * discount
+print(calculator(8,2, 0.15))
